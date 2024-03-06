2b75836ab9fc7c37ac574884ba8e1392d7f6c5ab
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-03-06 12:14:54 +0700

    level controller and update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
