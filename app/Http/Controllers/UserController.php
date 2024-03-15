89f833c979138e93f7860369cc3a056b28c55f0d
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-03-15 10:46:20 +0700

    js4-practicum2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('leve_id',2)->count();
-        dd($user);
-        return view ('user',['data'=>$user]);
+        $user = UserModel::first0rNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make ('12345'),
+            'level_id' => 2
+        ],
+    );
+    $user->save();
+    return view('user', ['data' => $user]);
     }
 }
