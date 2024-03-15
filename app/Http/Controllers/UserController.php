77a743c059d1b83b0c5e9e325f3bf2c5717426f1
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-03-15 10:25:05 +0700

    js4-practicum2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,19 +10,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        $user = UserModel::findOr(20,['username','nama'],function(){
+            abort(404);
+    });
 
-        UserModel::create($data);
-        
-        // coba akses model UserModel 
-        $user = UserModel::all(); // ambil semua data dari tabel m_user 
-        
-        return view('user', ['data' => $user]);
+    return view ('user',['data'=>$user]);
     }
 }
