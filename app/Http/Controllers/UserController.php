ddf70b2b7a460ad2f1448130201597356ec25e89
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-03-15 10:17:29 +0700

    js4-practicum1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,10 +12,13 @@ class UserController extends Controller
     {
         // tambah data user dengan Eloquent Model 
         $data = [
-            'nama' => 'Pelanggan Pertama', // Corrected the closing array bracket
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
 
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::create($data);
         
         // coba akses model UserModel 
         $user = UserModel::all(); // ambil semua data dari tabel m_user 
