ca9ec138fd0f03db86f2f784f35f5307b8e2cfa3
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-05-26 18:38:41 +0700

    js10-practicum 2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [RegisterController::class, '__invoke'])->name('register');
+Route::post('/login', [LoginController::class, '__invoke'])->name('login');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
