65c6429b288ef3595bce22cc0c1b52c1caf2e09a
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-05-27 08:30:20 +0700

    js11-assignments

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
@@ -9,52 +7,45 @@ use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         return BarangModel::all();
     }
 
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'price' => 'required|numeric|min:0',
-
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'kategori_id' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 400);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
 
-        $barang = BarangModel::create($request->all());
+        $image = $request->image;
+
+        $barang = BarangModel::create([
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'kategori_id' => $request->kategori_id,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $image->hashName(),
+        ]);
         return response()->json($barang, 201);
     }
 
-    public function show(BarangModel $barang)
-    {
-        return response()->json($barang);
+    public function show(BarangModel $barang) {
+        return BarangModel::find($barang);
     }
-
-    public function update(Request $request, BarangModel $barang)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'price' => 'required|numeric|min:0',
-
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 400);
-        }
-
+    public function update(Request $request, BarangModel $barang) {
         $barang->update($request->all());
-        return response()->json($barang);
+        return $barang;
     }
-
-    public function destroy(BarangModel $barang)
-    {
+    public function destroy(BarangModel $barang) {
         $barang->delete();
         return response()->json([
             'success' => true,
