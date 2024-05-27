34b591790811d54236c26ed71caac852707dc4ef
Lenka Melinda Florienka <lenkamelinda7@gmail.com>
2024-05-27 08:17:28 +0700

    js11-practicum 1

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\UserModel;
 use App\Http\Controllers\Controller;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -11,28 +11,29 @@ class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
-        // Set validation
         $validator = Validator::make($request->all(), [
             'username' => 'required',
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
-            'level_id' => 'required'
+            'level_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
-        // If validation fails
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        // Create user
+        $image = $request->image;
+        $image->store('public/posts');
+
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
+            'image' => $image->hashName(),
         ]);
 
-        // Return response JSON if user is created
         if ($user) {
             return response()->json([
                 'success' => true,
@@ -40,7 +41,6 @@ class RegisterController extends Controller
             ], 201);
         }
 
-        // Return JSON if process insert failed
         return response()->json([
             'success' => false,
         ], 409);
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,24 +2,44 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'image'
+    ];
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
-    public function getJWTCustomClaims()
+    public function getJWTCustomClaims(): array
     {
         return [];
     }
 
-    protected $table = "m_user";
-    protected $primaryKey = "user_id";
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    public function level()
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
+    }
 }
